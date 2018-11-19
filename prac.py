ea122f35950684e020b80cfa9e939b3aef7efbdf
Animesh Bohara <ani.bohara@gmail.com>
2018-11-19 22:21:53 +0530

    Structure tensor, Hessian matrix and Eigenvalues and Eigenvectors matrix created

--- prac.py
+++ prac.py
@@ -25,21 +25,69 @@ if __name__ == '__main__':
 	# plt.show()
 	############################
 
+
+
+
+
+	##################################################
 	# img is available
 	rows, cols, chans = img.shape
 
 	gradx = cv.Sobel(img, cv.CV_16S, 1, 0, ksize=1)
 	grady = cv.Sobel(img, cv.CV_16S, 0, 1, ksize=1)
-	grad = np.zeros((rows, cols, chans, 2))
-	grad[:, :, :, 0] = gradx
-	grad[:, :, :, 1] = grady 
+	# grad = np.zeros((rows, cols, chans, 2))
+	# grad[:, :, :, 0] = gradx
+	# grad[:, :, :, 1] = grady 
+
+	gradxsq = gradx * gradx
+	gradysq = grady * grady
+	gradxy = gradx * grady
+
+
+	## Computing Structure Tensor
 
 	G = np.zeros((rows, cols, 2, 2))
+
+	eig_value_large = np.zeros((rows, cols))
+	eig_value_small = np.zeros((rows, cols))
+
+	eig_vector_large = np.zeros((rows, cols, 2))
+	eig_vector_small = np.zeros((rows, cols, 2))
+
+	for chan in range(chans):
+		G[:, :, 0, 0] += gradxsq[:, :, chan]
+		G[:, :, 0, 1] += gradxy[:, :, chan]
+		G[:, :, 1, 0] += gradxy[:, :, chan]
+		G[:, :, 1, 1] += gradysq[:, :, chan]
+
+
+	## Computing Hessian Matrix of image
+
+	H = np.zeros((rows, cols, chans, 2, 2))
+
+	H[:, :, :, 0, 0] = cv.Sobel(gradx, cv.CV_16S, 1, 0, ksize=1)
+	H[:, :, :, 0, 1] = cv.Sobel(grady, cv.CV_16S, 1, 0, ksize=1)
+	H[:, :, :, 1, 0] = cv.Sobel(gradx, cv.CV_16S, 0, 1, ksize=1)
+	H[:, :, :, 1, 1] = cv.Sobel(grady, cv.CV_16S, 0, 1, ksize=1)
+
 	for row in range(rows):
 		for col in range(cols):
-			for chan in range(chans):
-				i_grad = grad[row, col, chan, :].reshape(2, 1)
-				# print(i_grad.shape)
-				G[row, col, :, :] += i_grad @ i_grad.T
+			G[row, col, :, :] = cv.GaussianBlur(G[row, col, :, :], (3, 3), 0)
+			eig_values, eig_vectors = np.linalg.eig(G[row, col, :, :])
+			if (eig_values[0] > eig_values[1]):
+				eig_value_large[row, col] = eig_values[0]
+				eig_value_small[row, col] = eig_values[1]
+				eig_vector_large[row, col, :] = eig_vectors[:, 0]
+				eig_vector_small[row, col, :] = eig_vectors[:, 1]
+			else:
+				eig_value_large[row, col] = eig_values[1]
+				eig_value_small[row, col] = eig_values[0]
+				eig_vector_large[row, col, :] = eig_vectors[:, 1]
+				eig_vector_small[row, col, :] = eig_vectors[:, 0]
 
+
+	
+
+
+	######################################################
 
